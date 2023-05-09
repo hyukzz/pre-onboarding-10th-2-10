05fcc5345a4b5dd27e156c91b387a44fa7a8f24c
hyukzz <ri_chard@naver.com>
2023-05-09 15:14:27 +0900

    refactor: api 호출 시 초기값 함수 전달

--- src/pages/SearchBar.js
+++ src/pages/SearchBar.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import useKeywordSuggestion from '../hooks/useKeywordSuggestion';
 import useInputChange from '../hooks/useInputChange';
-import useKeyboard from '../hooks/useKeyboard';
+import suggestKeyboardHandler from '../utils/suggestKeyboardHandler';
 import SearchBarInput from '../components/input/SearchBarInput';
 import SuggestionList from '../components/suggestion/SuggestionList';
 import Title from '../components/title/Title';
@@ -10,9 +10,10 @@ import './SearchBar.css';
 const SearchBar = () => {
   const [focusedIndex, setFocusedIndex] = useState(-1);
   const [keyword, handleInputChange, setKeyword] = useInputChange();
-  const [suggestions] = useKeywordSuggestion(keyword);
-
-  const handleKeyDown = useKeyboard(
+  const [suggestions] = useKeywordSuggestion(keyword, {
+    onSuccess: () => setFocusedIndex(-2),
+  });
+  const handleKeyDown = suggestKeyboardHandler(
     suggestions,
     focusedIndex,
     setFocusedIndex,
